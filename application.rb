436d0135a4f34c542416a932e113a81de25cb125
minoritea <minorityland@gmail.com>
2014-11-16 12:15:37 +0900

    retab

--- application.rb
+++ application.rb
@@ -5,20 +5,20 @@ require 'json'
 $notes = ['Input comments...']
 
 helpers do
-	def load_script(filename)
-		::IO.read(
-			File.expand_path(
-				File.join('../public/javascripts', filename),
-				__FILE__))
-	end
+  def load_script(filename)
+    ::IO.read(
+      File.expand_path(
+        File.join('../public/javascripts', filename),
+        __FILE__))
+  end
 
-	def react(jsx)
-		if ReactRuby.renderer.nil?
-			ReactRuby.compile(
-				jsx: load_script('view.jsx')
-			)
-		end
-		ReactRuby.render(jsx)
+  def react(jsx)
+    if ReactRuby.renderer.nil?
+      ReactRuby.compile(
+        jsx: load_script('view.jsx')
+      )
+    end
+    ReactRuby.render(jsx)
   end
 end
 
@@ -31,16 +31,16 @@ get '/' do
   erb <<-ERB
     <html>
       <head>
-			  <script src='/javascripts/vendor/react.js'></script>
-			  <script src='/javascripts/vendor/JSXTransformer.js'></script>
-			  <script src='/javascripts/vendor/vendor.js'></script>
-				<script type='text/jsx' src='/javascripts/view.jsx'></script>
-				<script type='text/jsx' src='/javascripts/app.jsx'></script>
+        <script src='/javascripts/vendor/react.js'></script>
+        <script src='/javascripts/vendor/JSXTransformer.js'></script>
+        <script src='/javascripts/vendor/vendor.js'></script>
+        <script type='text/jsx' src='/javascripts/view.jsx'></script>
+        <script type='text/jsx' src='/javascripts/app.jsx'></script>
       </head>
       <body>
-			  <div id='notePad'>
-					<%= react(%(<NotePad notes={#{$notes.to_json}}/>)) %>
-				</div>
+        <div id='notePad'>
+          <%= react(%(<NotePad notes={#{$notes.to_json}}/>)) %>
+        </div>
         <form action='#'>
           <input type='text' name='note'></input>
           <button type='submit'>Add</button>
